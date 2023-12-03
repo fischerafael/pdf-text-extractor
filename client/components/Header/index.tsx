83740eadd32e0b0572a463d2110b3b1a53542a30
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-12-02 22:37:52 -0300

    feat: working on responsibility

--- client/components/Header/index.tsx
+++ client/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { theme } from "@/client/config/theme";
 import * as C from "@chakra-ui/react";
 import { Button } from "../Button";
 import { Logo } from "../Logo";
+import * as Icon from "react-icons/hi";
 
 interface HeaderProps {
   logoSlot: React.ReactNode;
@@ -10,23 +11,38 @@ interface HeaderProps {
   isTransparent?: boolean;
 }
 
-export const Header = ({
-  logoSlot,
-  linksSlot,
-  actionSlot,
-  isTransparent = true,
-}: HeaderProps) => {
+export const Header = ({ isTransparent = true, ...props }: HeaderProps) => {
   return (
     <C.HStack
       w="full"
       justify="center"
       bg={isTransparent ? "transparent" : `${theme.mainColour}`}
     >
-      <C.HStack w="full" h="10vh" justify="space-between" maxW="container.xl">
-        {logoSlot}
-        {linksSlot}
-        {actionSlot}
-      </C.HStack>
+      <MobileHeader {...props} />
+    </C.HStack>
+  );
+};
+
+const WebHeader = ({ logoSlot, linksSlot, actionSlot }: HeaderProps) => {
+  return (
+    <C.HStack w="full" h="10vh" justify="space-between" maxW="container.xl">
+      {logoSlot}
+      {linksSlot}
+      {actionSlot}
+    </C.HStack>
+  );
+};
+
+const MobileHeader = ({ logoSlot, linksSlot, actionSlot }: HeaderProps) => {
+  const { isOpen, onClose, onOpen } = C.useDisclosure();
+  return (
+    <C.HStack justify="space-between" h="10vh" w="full">
+      {logoSlot}
+      <C.IconButton
+        aria-label="Hamburguer Menu"
+        icon={<Icon.HiOutlineMenu />}
+        colorScheme={theme.accentColour}
+      />
     </C.HStack>
   );
 };
