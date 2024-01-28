5cc7c1b34f22984e7a4c520d55a893dd1f3571fb
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-01-28 19:26:41 -0300

    feat: rendering tasks of last week

--- client/pages/PageAppReports/hook/usePageAppReports.tsx
+++ client/pages/PageAppReports/hook/usePageAppReports.tsx
@@ -1,14 +1,28 @@
 import { tasksGateway } from "@/client/gateways/api/tasks";
 import { useAuthentication } from "@/client/hooks/global/useAuthenticationGlobal";
+import { useQuery } from "react-query";
 
 export const usePageAppReports = () => {
   const { presenters } = useAuthentication();
 
-  const getLastWeekData = async () => {
-    try {
-      const data = await tasksGateway.getLasWeekTasks(presenters.email);
-    } catch (e: any) {}
-  };
+  const { data } = useQuery({
+    queryKey: ["reports", presenters.email],
+    queryFn: () => tasksGateway.getLasWeekTasks(presenters.email),
+    enabled: !!presenters.email,
+  });
 
-  return { controllers: { getLastWeekData }, presenters: {} };
+  // const getLastWeekData = async () => {
+  //   try {
+  //     const data = await tasksGateway.getLasWeekTasks(presenters.email);
+  //   } catch (e: any) {}
+  // };
+
+  console.log("[queries]", data);
+
+  return {
+    controllers: {},
+    presenters: {
+      tasks: data,
+    },
+  };
 };
--- client/pages/PageAppReports/index.tsx
+++ client/pages/PageAppReports/index.tsx
@@ -6,6 +6,8 @@ import * as C from "@chakra-ui/react";
 import * as Icon from "react-icons/hi";
 import { Text } from "@/client/components/Text";
 import { usePageAppReports } from "./hook/usePageAppReports";
+import { utils } from "@/client/utils";
+import { Button } from "@/client/components/Button";
 
 export const PageAppReports = () => {
   const { controllers, presenters } = usePageAppReports();
@@ -18,34 +20,23 @@ export const PageAppReports = () => {
       mainSlot={
         <C.VStack w="full" align="center" p="8">
           <C.VStack w="full" maxW="container.sm" align="flex-start" gap="8">
-            <Text fontSize="xl">Reports</Text>
-
-            <C.VStack w="full" align="flex-start">
-              <C.HStack
-                w="full"
-                bg="white"
-                p="8"
-                border="1px"
-                borderColor="gray.200"
-                cursor="pointer"
-                onClick={controllers.getLastWeekData}
-              >
-                <C.VStack w="full" align="flex-start">
-                  <C.Icon
-                    w="6"
-                    h="6"
-                    color="purple.600"
-                    as={Icon.HiOutlineDocumentReport}
-                  />
-                  <Text>Last Week</Text>
+            <C.HStack w="full" justify="space-between">
+              <Text fontSize="xl">Last 7 Days</Text>
+              <Button>Download CSV</Button>
+            </C.HStack>
+            <C.VStack w="full" spacing="8">
+              {presenters.tasks?.map((task) => (
+                <C.VStack key={task.dayOfTheWeek} w="full" align="flex-start">
+                  <C.HStack w="full" justify="space-between">
+                    <Text variant="h3">{task.dayOfTheWeek}</Text>
+                  </C.HStack>
+                  <C.VStack w="full" align="flex-start">
+                    {task.data.map((tk) => (
+                      <Text key={tk.id}>{tk.details.task}</Text>
+                    ))}
+                  </C.VStack>
                 </C.VStack>
-                <C.Icon
-                  w="4"
-                  h="4"
-                  color="purple.600"
-                  as={Icon.HiOutlineChevronRight}
-                />
-              </C.HStack>
+              ))}
             </C.VStack>
           </C.VStack>
         </C.VStack>
@@ -53,3 +44,28 @@ export const PageAppReports = () => {
     />
   );
 };
+
+{
+  /* <C.VStack w="full" align="flex-start">
+  <C.HStack
+    w="full"
+    bg="white"
+    p="8"
+    border="1px"
+    borderColor="gray.200"
+    cursor="pointer"
+    onClick={controllers.getLastWeekData}
+  >
+    <C.VStack w="full" align="flex-start">
+      <C.Icon
+        w="6"
+        h="6"
+        color="purple.600"
+        as={Icon.HiOutlineDocumentReport}
+      />
+      <Text>Last Week</Text>
+    </C.VStack>
+    <C.Icon w="4" h="4" color="purple.600" as={Icon.HiOutlineChevronRight} />
+  </C.HStack>
+</C.VStack>; */
+}
