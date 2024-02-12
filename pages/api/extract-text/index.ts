f319bee37347382d21d01de0c4815ca2e8f74aec
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-02-12 18:42:33 -0300

    feat: moved extractor

--- pages/api/extract-text/index.ts
+++ pages/api/extract-text/index.ts
@@ -1,3 +1,4 @@
+import { pdfExtractor } from "@/server/PDFTextExtractor";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export const config = {
@@ -19,38 +20,3 @@ export default async function handler(req: Request, res: NextApiResponse) {
     return res.status(500).json({ error: e.message });
   }
 }
-
-import multer from "multer";
-import pdfParse from "pdf-parse";
-import fs from "fs";
-
-class TextExtractor {
-  private upload = multer({
-    storage: multer.diskStorage({}),
-    limits: { fileSize: 20 * 1024 * 1024 }, // Limit file size to 1MB
-  });
-
-  uploadPDFTemp = async (req: any, res: any) => {
-    await new Promise((resolve, reject) => {
-      this.upload.single("file")(req as any, res as any, (err: any) => {
-        if (err instanceof multer.MulterError) {
-          console.error(err);
-          return reject({ status: 400, message: "File upload error" });
-        }
-        if (err) {
-          console.error(err);
-          return reject({ status: 500, message: "Internal server error" });
-        }
-        resolve("");
-      });
-    });
-  };
-
-  parsePDFFile = async (file: any) => {
-    const fileBuffer = fs.readFileSync(file.path);
-    const data = await pdfParse(fileBuffer);
-    return data;
-  };
-}
-
-export const pdfExtractor = new TextExtractor();
