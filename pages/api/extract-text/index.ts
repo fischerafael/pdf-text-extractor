302cd318de5da811b339e287a5b82153b4322d0c
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-02-12 18:39:11 -0300

    feat: parsing data

--- pages/api/extract-text/index.ts
+++ pages/api/extract-text/index.ts
@@ -1,13 +1,56 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+interface Request extends NextApiRequest {
+  file: any;
+}
+
+export default async function handler(req: Request, res: NextApiResponse) {
   try {
-    const { method } = req;
-    return res.status(200).json({ name: "John Doe" });
+    await pdfExtractor.uploadPDFTemp(req, res);
+    const data = await pdfExtractor.parsePDFFile(req.file);
+    return res.status(200).json({ data: data });
   } catch (e: any) {
     return res.status(500).json({ error: e.message });
   }
 }
+
+import multer from "multer";
+import pdfParse from "pdf-parse";
+import fs from "fs";
+
+class TextExtractor {
+  private upload = multer({
+    storage: multer.diskStorage({}),
+    limits: { fileSize: 20 * 1024 * 1024 }, // Limit file size to 1MB
+  });
+
+  uploadPDFTemp = async (req: any, res: any) => {
+    await new Promise((resolve, reject) => {
+      this.upload.single("file")(req as any, res as any, (err: any) => {
+        if (err instanceof multer.MulterError) {
+          console.error(err);
+          return reject({ status: 400, message: "File upload error" });
+        }
+        if (err) {
+          console.error(err);
+          return reject({ status: 500, message: "Internal server error" });
+        }
+        resolve("");
+      });
+    });
+  };
+
+  parsePDFFile = async (file: any) => {
+    const fileBuffer = fs.readFileSync(file.path);
+    const data = await pdfParse(fileBuffer);
+    return data;
+  };
+}
+
+export const pdfExtractor = new TextExtractor();
