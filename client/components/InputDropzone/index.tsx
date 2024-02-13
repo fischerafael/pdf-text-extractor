b89dbc23e6558b34bc93b796139da9e42dc725fe
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-02-13 18:05:15 -0300

    feat: extracting text

--- client/components/InputDropzone/index.tsx
+++ client/components/InputDropzone/index.tsx
@@ -1,19 +1,26 @@
 import React from "react";
+import * as Chakra from "@chakra-ui/react";
 import DropZone from "react-dropzone";
 
 export const InputDropzone = ({ onDrop }: { onDrop: (e: any) => void }) => {
   return (
     <DropZone onDrop={onDrop}>
       {({ getRootProps, getInputProps }) => (
-        <section>
-          <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-          </div>
-        </section>
+        <Chakra.VStack w="full" justify="center">
+          <Chakra.VStack
+            h="20vh"
+            w="full"
+            justify="center"
+            border="dashed 1px"
+            borderRadius="8"
+            borderColor="gray.200"
+            {...getRootProps()}
+          >
+            <Chakra.Input size={10 as any} {...getInputProps()} />
+            <Chakra.Text>Select or Drag and Drop a PDF</Chakra.Text>
+          </Chakra.VStack>
+        </Chakra.VStack>
       )}
     </DropZone>
   );
 };
-
-// (acceptedFiles) => console.log(acceptedFiles)}
--- client/pages/PageExtractText/index.tsx
+++ client/pages/PageExtractText/index.tsx
@@ -1,15 +1,71 @@
 import { InputDropzone } from "@/client/components/InputDropzone";
-import React from "react";
+import React, { useState } from "react";
+import * as Chakra from "@chakra-ui/react";
+import { Button } from "@/client/components/Button";
+import axios from "axios";
 
 export const PageExtractText = () => {
-  const onDrop = (acceptedFiles: any) => {
-    alert("hi");
-    console.log(acceptedFiles);
+  const [state, setState] = useState<{ isLoading: boolean; file: null | File }>(
+    {
+      file: null,
+      isLoading: false,
+    }
+  );
+
+  const onDrop = (acceptedFiles: File[]) => {
+    setState((prev) => ({ ...prev, file: acceptedFiles[0] }));
+  };
+
+  const onRemoveFile = () => {
+    setState((prev) => ({ ...prev, file: null }));
+  };
+
+  console.log("[file]", state);
+
+  const onExtractText = async () => {
+    setState((prev) => ({ ...prev, isLoading: true }));
+    try {
+      const form = new FormData();
+      form.append("file", state.file as File);
+      const { data } = await axios.post<{
+        data: {
+          text: string;
+        };
+      }>("/api/extract-text", form);
+      console.log("[response]", data.data.text);
+    } catch (e: any) {
+      console.log("[error]", e.message);
+    } finally {
+      setState((prev) => ({ ...prev, isLoading: false }));
+    }
   };
 
   return (
-    <div>
-      <InputDropzone onDrop={onDrop} />
-    </div>
+    <Chakra.VStack w="full" p="8">
+      {!state.file && <InputDropzone onDrop={onDrop} />}
+      {!!state.file && (
+        <Chakra.VStack w="full" align="flex-end">
+          <Chakra.HStack
+            w="full"
+            bg="gray.50"
+            borderRadius="8"
+            p="16"
+            border="1px"
+            borderColor="gray.200"
+            justify="space-between"
+            h="20vh"
+          >
+            <Chakra.Text>{state.file?.name}</Chakra.Text>
+            <Button variant="outline" bg="transparent" onClick={onRemoveFile}>
+              Remove File
+            </Button>
+          </Chakra.HStack>
+
+          <Button isLoading={state.isLoading} onClick={onExtractText}>
+            Extract Text
+          </Button>
+        </Chakra.VStack>
+      )}
+    </Chakra.VStack>
   );
 };
